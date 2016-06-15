01779d365b7ba90e1c7223b9cdf5ec04d626a7b4
rpvaidya <raghavendra.vaidya@gmail.com>
2016-06-15 10:04:30 +0530

    Added Data Class for Accounts

---
+++ src/main/java/com/vaidya/Accounts.java
@@ -0,0 +1,91 @@
+package com.vaidya;
+
+public class Accounts {
+
+	private String belongs;
+	private String number;
+	private String category;
+	private String description;
+	private String valid_from;
+	private String valid_to;
+	private String address;
+	private String name;
+	private String web_address;
+	private String uid;
+	private String pwd;
+	private String hash_key;
+
+	public String getBelongs() {
+		return belongs;
+	}
+	public void setBelongs(String belongs) {
+		this.belongs = belongs;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getValid_from() {
+		return valid_from;
+	}
+	public void setValid_from(String valid_from) {
+		this.valid_from = valid_from;
+	}
+	public String getValid_to() {
+		return valid_to;
+	}
+	public void setValid_to(String valid_to) {
+		this.valid_to = valid_to;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getWeb_address() {
+		return web_address;
+	}
+	public void setWeb_address(String web_address) {
+		this.web_address = web_address;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getHash_key() {
+		return hash_key;
+	}
+	public void setHash_key(String hash_key) {
+		this.hash_key = hash_key;
+	}
+	
+}
